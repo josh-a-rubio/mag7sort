f2db9bfc6fff9e145f53546a0746c415eaaa937b
Josh Rubio <joshrubio@Josh-Book.local>
2025-12-17 15:44:29 -0500

    Hard code testing and set merge sort recursion

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define TICKER 5
+#define TICKER 6
 #define STOCKS 7
 typedef struct Stock {
     char ticker[TICKER];
@@ -29,20 +29,33 @@ int main(void)
 }
 //Fetch mag7 EOD data
 void data_setup(Stock mag7[], int count) {
+    if (count < 7) return;
+
+    strcpy(mag7[0]. ticker, "MSFT"); mag7[0].price = 299.10;
+    strcpy(mag7[1]. ticker, "AAPL"); mag7[1].price = 207.30;
+    strcpy(mag7[2]. ticker, "TSLA"); mag7[2].price = 174.50;
+    strcpy(mag7[3]. ticker, "GOOGL"); mag7[3].price = 131.20;
+    strcpy(mag7[4]. ticker, "AMZN"); mag7[4].price = 102.00;
+    strcpy(mag7[5]. ticker, "META"); mag7[5].price = 184.60;
+    strcpy(mag7[6]. ticker, "NVDA"); mag7[6].price = 441.00;
     
-
-
-
 }
-
 
 //Merge sort algo
 void merge_sort(Stock mag7[], int left, int right) {
+    if(left >= right) return;
+    
+    int mid = left + (right - left) / 2;
+
+    merge_sort(mag7, left, mid);
+    merge_sort(mag7, mid + 1, right);
+    merge(mag7, left, mid, right);
 
 }
 
 //Merge helper
 void merge(Stock mag7[], int left, int mid, int right) {
+    
 
 }
 
